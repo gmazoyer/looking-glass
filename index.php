bf2d8211d6bac1cac602e8ea991c86460a312884
Guillaume Mazoyer <gmazoyer@gravitons.in>
2016-02-08 19:44:23 +0100

    Update jQuery to 2.2.0.

--- index.php
+++ index.php
@@ -237,7 +237,7 @@ final class LookingGlass {
     $this->render_footer();
     $this->render_help_modal();
     print('</body>');
-    print('<script src="libs/jquery-2.1.4.min.js"></script>');
+    print('<script src="libs/jquery-2.2.0.min.js"></script>');
     print('<script src="libs/bootstrap-3.3.6/js/bootstrap.min.js"></script>');
     print('<script src="js/looking-glass.js"></script>');
     print('</html>');
