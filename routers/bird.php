1490ced865c70d9a42a3b6950aa2fc213efafc24
Guillaume Mazoyer <gmazoyer@gravitons.in>
2017-01-03 23:39:07 +0100

    Copyright updated to 2017.

--- routers/bird.php
+++ routers/bird.php
@@ -2,7 +2,7 @@
 
 /*
  * Looking Glass - An easy to deploy Looking Glass
- * Copyright (C) 2014-2016 Guillaume Mazoyer <gmazoyer@gravitons.in>
+ * Copyright (C) 2014-2017 Guillaume Mazoyer <gmazoyer@gravitons.in>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
