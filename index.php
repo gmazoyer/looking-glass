c457d050253f68c2677530f158c2f78d240e1fb0
Guillaume Mazoyer <gmazoyer@gravitons.in>
2018-07-23 23:04:03 +0200

    Update Font Awesome to 5.1.1.

--- index.php
+++ index.php
@@ -300,7 +300,7 @@ final class LookingGlass {
     print('</body>');
     print('<script src="libs/jquery-3.3.1.min.js"></script>');
     print('<script src="libs/bootstrap-4.1.2/js/bootstrap.min.js"></script>');
-    print('<script src="libs/fontawesome-5.0.13/js/fontawesome-all.min.js"></script>');
+    print('<script src="libs/fontawesome-5.1.1/js/all.min.js"></script>');
     print('<script src="js/looking-glass.js"></script>');
     if ($this->recaptcha['enabled'] && isset($this->recaptcha['apikey']) && isset($this->recaptcha['secret'])) {
       print('<script src="https://www.google.com/recaptcha/api.js" async defer></script>');
