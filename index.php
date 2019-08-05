2131cdd9cd8b9ebf3b59d98c00d67a7d5893e376
Guillaume Mazoyer <guillaume@mazoyer.eu>
2019-08-05 20:04:35 +0200

    Upgrade jQuery to 3.4.1 and Font Awesome to 5.10.1.

--- index.php
+++ index.php
@@ -298,9 +298,9 @@ final class LookingGlass {
       $this->render_peering_policy_modal();
     }
     print('</body>');
-    print('<script src="libs/jquery-3.3.1.min.js"></script>');
+    print('<script src="libs/jquery-3.4.1.min.js"></script>');
     print('<script src="libs/bootstrap-4.3.1/js/bootstrap.min.js"></script>');
-    print('<script src="libs/fontawesome-5.8.2/js/all.min.js"></script>');
+    print('<script src="libs/fontawesome-5.10.1/js/all.min.js"></script>');
     print('<script src="js/looking-glass.js"></script>');
     if ($this->recaptcha['enabled'] && isset($this->recaptcha['apikey']) && isset($this->recaptcha['secret'])) {
       print('<script src="https://www.google.com/recaptcha/api.js" async defer></script>');
