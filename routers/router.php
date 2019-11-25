6f2428e18acb6b1cd66a544dcc10444127c8cd40
rabin-io <rabin@rabin.io>
2019-11-25 22:11:22 +0200

    Add support for Arista

--- routers/router.php
+++ routers/router.php
@@ -21,6 +21,7 @@
 
 require_once('includes/config.defaults.php');
 require_once('config.php');
+require_once('arista.php');
 require_once('bird.php');
 require_once('cisco.php');
 require_once('cisco_iosxr.php');
@@ -222,6 +223,9 @@ abstract class Router {
     $router_config = $config['routers'][$id];
 
     switch (strtolower($router_config['type'])) {
+      case 'arista':
+        return new Arista($config, $router_config, $id, $requester);
+
       case 'bird':
         return new Bird($config, $router_config, $id, $requester);
 
