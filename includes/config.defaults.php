452d742e913fbd751911fe53cfffe3668decc9ba
Guillaume Mazoyer <guillaume@mazoyer.eu>
2021-02-27 12:01:24 +0100

    Release 2.1.0

--- includes/config.defaults.php
+++ includes/config.defaults.php
@@ -46,7 +46,7 @@ $config = array(
 
   // Release configuration
   'release' => array(
-    'version' => '2.0.1',
+    'version' => '2.1.0',
     'codename' => 'Established',
     'repository' => 'https://github.com/respawner/looking-glass'
   ),
